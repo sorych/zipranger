1de540024f6863750711e2c951d4758b954c59c6
sorych <dmytro.sorych@osi.ca.gov>
2019-06-19 09:02:52 -0700

    logger added

--- src/main/java/com/sorych/zipranger/reader/ConsoleZipcodeRangeReader.java
+++ src/main/java/com/sorych/zipranger/reader/ConsoleZipcodeRangeReader.java
@@ -7,15 +7,14 @@ import java.util.Scanner;
 
 public class ConsoleZipcodeRangeReader implements ZipRangesReader {
 
-  Queue<String> queue = new ArrayDeque<>();
-
-  private final String END_INPUT_STRING = "X";
-
+  private final static String END_INPUT_STRING = "X";
+  private Queue<String> queue = new ArrayDeque<>();
 
   @Override
   public String getNextZipRangeString() {
     if (queue.peek() == null) {
-      System.out.println("please insert next line or type 'X' to finish input");
+      System.out
+          .println("please insert next line or type " + END_INPUT_STRING + " to finish input");
       Scanner scanner = new Scanner(System.in);
       String inputString = scanner.nextLine();
       if (END_INPUT_STRING.equals(inputString)) {
--- src/main/java/com/sorych/zipranger/util/ZipRangeUtil.java
+++ src/main/java/com/sorych/zipranger/util/ZipRangeUtil.java
@@ -4,11 +4,15 @@ import static com.sorych.zipranger.ZipRange.fromInt;
 
 import com.sorych.zipranger.ZipRange;
 import java.util.StringTokenizer;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class ZipRangeUtil {
 
   private String zipRangeRegex;
   private String zipRangeDelim;
+
+  final static Log LOGGER = LogFactory.getLog(ZipRangeUtil.class);
 
   public ZipRangeUtil(String zipRangeRegex, String zipRangeDelim) {
     this.zipRangeRegex = zipRangeRegex;
@@ -17,6 +21,7 @@ public class ZipRangeUtil {
 
   public void validate(ZipRange zipRange) {
     if (zipRange.end < zipRange.begin) {
+      LOGGER.error("wrong zip range format, begin > end: " + zipRange);
       throw new IllegalArgumentException("wrong zip range format, begin > end: " + zipRange);
     }
   }
@@ -27,8 +32,9 @@ public class ZipRangeUtil {
     }
     StringTokenizer multiTokenizer = new StringTokenizer(value, zipRangeDelim);
     if (multiTokenizer.countTokens() < 2) {
-      throw new IllegalStateException(
-          String.format("zip range cannot be tokenized using %s delimeter", zipRangeDelim));
+      String errormsg = String.format("zip range cannot be tokenized using %s delimeter", zipRangeDelim);
+      LOGGER.error(errormsg);
+      throw new IllegalStateException(errormsg);
     }
     return fromInt(Integer.parseInt(multiTokenizer.nextToken()),
         Integer.parseInt(multiTokenizer.nextToken()));
@@ -40,15 +46,15 @@ public class ZipRangeUtil {
 
   /**
    * merges two ZipRange to one if overlap
-   * @param zr1
-   * @param zr2
+   *
    * @return the ZipRange result of the merge, null if not overlapping
    */
   public ZipRange merge(ZipRange zr1, ZipRange zr2) {
     if (!overlap(zr1, zr2)) {
       return null;
     }
-    return fromInt(zr1.begin > zr2.begin ? zr2.begin : zr1.begin, zr1.end > zr2.end ? zr1.end : zr2.end);
+    return fromInt(zr1.begin > zr2.begin ? zr2.begin : zr1.begin,
+        zr1.end > zr2.end ? zr1.end : zr2.end);
   }
 }
 
